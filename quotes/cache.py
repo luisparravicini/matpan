415fe0ec3f6fc07789aba712ac2e33ab1b425ff1
Luis Parravicini <lparravi@gmail.com>
2019-09-06 14:34:03 -0300

    fetching prices

--- quotes/cache.py
+++ quotes/cache.py
@@ -1,9 +1,13 @@
 import os
 # import shutil
 import hashlib
+import json
 
 
 class Cache:
+
+    GET_METHOD = 'get'
+    POST_METHOD = 'post'
 
     def __init__(self, path):
         self._cache_path = path
@@ -12,20 +16,21 @@ class Cache:
         if not os.path.isdir(path):
             os.mkdir(path)
 
-    def save(self, url, data):
-        path = self._to_fname(url)
+    def save(self, method, url, data, params=None):
+        path = self._to_fname(method, url, params)
         with open(path, 'w') as file:
             file.write(data)
 
-    def try_get(self, url):
-        path = self._to_fname(url)
+    def try_get(self, method, url, params=None):
+        path = self._to_fname(method, url, params)
         data = None
         if os.path.isfile(path):
             with open(path, 'r') as file:
                 data = file.read()
         return data
 
-    def _to_fname(self, url):
+    def _to_fname(self, method, url, params):
         global _cache_path
-        digest = hashlib.sha256(url.encode('utf-8')).hexdigest()
+        s = json.dumps([method, url, params])
+        digest = hashlib.sha256(s.encode('utf-8')).hexdigest()
         return os.path.join(self._cache_path, digest)
--- quotes/quotes.py
+++ quotes/quotes.py
@@ -5,17 +5,28 @@ from cache import Cache
 import re
 import yaml
 from random import shuffle
+from datetime import datetime
 
 
 conf = None
 
 
 def get_url(url):
-    print('fetching', url)
+    print(f'fetching {url}')
     response = requests.get(url)
     status = response.status_code
     if status != 200:
-        print('Unexpected response status: ' + status)
+        print(f'Unexpected response status: {status}')
+        sys.exit(1)
+    return response
+
+
+def post_url(url, req_data):
+    print(f'fetching {url} with {req_data}')
+    response = requests.post(url, data=req_data)
+    status = response.status_code
+    if status != 200:
+        print(f'Unexpected response status: {status}')
         sys.exit(1)
     return response
 
@@ -32,6 +43,15 @@ def _save_conf():
         yaml.safe_dump(conf, file)
 
 
+def _get_fetch(url):
+    data = cache.try_get(Cache.GET_METHOD, url)
+    if data is None:
+        response = get_url(url)
+        data = response.text
+        cache.save(url, Cache.GET_METHOD, data)
+    return data
+
+
 def _setup_symbols():
     global conf
     symbols_key = 'symbols'
@@ -40,12 +60,7 @@ def _setup_symbols():
         return
 
     symbols_url = conf['symbols_url']
-    data = cache.try_get(symbols_url)
-    if data is None:
-        response = get_url(url)
-        data = response.text
-        cache.save(url, data)
-
+    data = _get_fetch(symbols_url)
     symbols = list()
     soup = BeautifulSoup(data, 'html.parser')
     for link in soup.findAll('a'):
@@ -54,12 +69,12 @@ def _setup_symbols():
         if match is not None:
             id = match.group(1)
             symbol = link.text.strip()
-            symbol = symbol.split("\n")[0]
+            symbol = symbol.split("\n")[0].strip()
             name = link['title']
             symbols.append((id, symbol, name))
 
     conf[symbols_key] = symbols
-    print('parsed', len(symbols), 'symbols')
+    print(f'parsed {len(symbols)} symbols')
     _save_conf()
 
 
@@ -67,10 +82,33 @@ def _update_prices():
     global conf
 
     symbols = conf['symbols'].copy()
-    shuffle(symbols)
+    # shuffle(symbols)
     for datum in symbols:
         id, symbol, _ = datum
-        print(id, symbol)
+        print(symbol)
+
+        start_date = datetime.strptime('2018-01-01', '%Y-%m-%d')
+        end_date = datetime.strptime('2019-09-01', '%Y-%m-%d')
+
+        date_range = " - ".join([
+            start_date.strftime('%d/%m/%Y'),
+            end_date.strftime('%d/%m/%Y'),
+        ])
+        req_data = dict()
+        params = conf['price_params']
+        req_data[params['dates']] = date_range
+        req_data[params['id']] = id
+        req_data = {**req_data, **params['extras']}
+
+        price_url = conf['price_url']
+        data = cache.try_get(Cache.POST_METHOD, price_url, req_data)
+        if data is None:
+            response = post_url(price_url, req_data)
+            data = response.text
+            cache.save(Cache.POST_METHOD, price_url, data, params=req_data)
+
+        # print(data)
+
 
 
 if __name__ == '__main__':
