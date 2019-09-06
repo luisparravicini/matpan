9e4ae4d5aed18d318068db31ea544c1725f12dfe
Luis Parravicini <lparravi@gmail.com>
2019-09-06 17:53:06 -0300

    http requests inside Downloader class

--- quotes/quotes.py
+++ quotes/quotes.py
@@ -9,26 +9,47 @@ from datetime import datetime
 
 
 conf = None
+downloader = None
 
 
-def get_url(url):
-    print(f'fetching {url}')
-    response = requests.get(url)
-    status = response.status_code
-    if status != 200:
-        print(f'Unexpected response status: {status}')
-        sys.exit(1)
-    return response
+class Downloader:
 
+    def __init__(self, cache_dir):
+        self.cache = Cache(cache_dir)
 
-def post_url(url, req_data):
-    print(f'fetching {url} with {req_data}')
-    response = requests.post(url, data=req_data)
-    status = response.status_code
-    if status != 200:
-        print(f'Unexpected response status: {status}')
-        sys.exit(1)
-    return response
+    def get(self, url):
+        data = self.cache.try_get(Cache.GET_METHOD, url)
+        if data is None:
+            response = self._do_get(url)
+            data = response.text
+            self.cache.save(url, Cache.GET_METHOD, data)
+        return data
+
+    def post(self, url, req_data):
+        data = self.cache.try_get(Cache.POST_METHOD, url, req_data)
+        if data is None:
+            response = self._do_post(url, req_data)
+            data = response.text
+            self.cache.save(Cache.POST_METHOD, url, data, params=req_data)
+        return data
+
+    def _do_get(self, url):
+        print(f'fetching {url}')
+        response = requests.get(url)
+        self._check_is_ok(response)
+        return response
+
+    def _do_post(self, url, req_data):
+        print(f'fetching {url} with {req_data}')
+        response = requests.post(url, data=req_data)
+        self._check_is_ok(response)
+        return response
+
+    def _check_is_ok(self, response):
+        status = response.status_code
+        if status != 200:
+            print(f'Unexpected response status: {status}')
+            sys.exit(1)
 
 
 def _read_conf():
@@ -43,24 +64,16 @@ def _save_conf():
         yaml.safe_dump(conf, file)
 
 
-def _get_fetch(url):
-    data = cache.try_get(Cache.GET_METHOD, url)
-    if data is None:
-        response = get_url(url)
-        data = response.text
-        cache.save(url, Cache.GET_METHOD, data)
-    return data
-
-
 def _setup_symbols():
     global conf
+    global downloader
     symbols_key = 'symbols'
 
     if symbols_key in conf:
         return
 
     symbols_url = conf['symbols_url']
-    data = _get_fetch(symbols_url)
+    data = downloader.get(symbols_url)
     symbols = list()
     soup = BeautifulSoup(data, 'html.parser')
     for link in soup.findAll('a'):
@@ -80,6 +93,7 @@ def _setup_symbols():
 
 def _update_prices():
     global conf
+    global downloader
 
     symbols = conf['symbols'].copy()
     shuffle(symbols)
@@ -101,18 +115,13 @@ def _update_prices():
         req_data = {**req_data, **params['extras']}
 
         price_url = conf['price_url']
-        data = cache.try_get(Cache.POST_METHOD, price_url, req_data)
-        if data is None:
-            response = post_url(price_url, req_data)
-            data = response.text
-            cache.save(Cache.POST_METHOD, price_url, data, params=req_data)
+        data = downloader.post(price_url, req_data)
 
         # print(data)
 
 
-
 if __name__ == '__main__':
-    cache = Cache('cache')
+    downloader = Downloader('cache')
     _read_conf()
     _setup_symbols()
     _update_prices()
