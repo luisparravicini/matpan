d835f7e3b6bcc60999fa569f5e837eb75804a57d
Luis Parravicini <lparravi@gmail.com>
2019-09-08 06:51:39 -0300

    moved Prices to it's own file

--- quotes/quotes.py
+++ quotes/quotes.py
@@ -7,32 +7,13 @@ from datetime import datetime, date, timedelta
 from downloader import Downloader
 import os
 import pandas as pd
-
+from prices import Prices
 
 conf = None
 downloader = None
 prices_manager = None
 
 
-class Prices:
-    EXT = '.csv'
-
-    def __init__(self, base_dir, se):
-        self.se = se
-        self.prices_path = os.path.join(base_dir, se)
-        os.makedirs(self.prices_path, exist_ok=True)
-
-    def save(self, symbol, data):
-        path = os.path.join(self.prices_path, symbol + Prices.EXT)
-        data.to_csv(path, index_label='Date')
-
-    def load(self, symbol):
-        path = os.path.join(self.prices_path, symbol + Prices.EXT)
-        if os.path.exists(path):
-            return pd.read_csv(path,
-                               parse_dates=True,
-                               index_col='Date')
-        return None
 
 def _read_conf():
     global conf
