4411a0414618aacd0f6bc885ad9c31dd6add5cf3
Luis Parravicini <lparravi@gmail.com>
2019-11-29 09:58:20 -0300

    moved price loader functions to a separate file

--- quotes/find_strategy.py
+++ quotes/find_strategy.py
@@ -8,40 +8,7 @@ from plot import plot
 from conf import Configuration
 from prices import Prices
 from datetime import date, timedelta
-
-
-prices_manager = None
-
-
-def load_data(start_date, end_date, symbol):
-    global prices_manager
-
-    data = prices_manager.load(symbol)
-    all_weekdays = pd.date_range(start=start_date, end=end_date, freq='B')
-    data = data.reindex(all_weekdays).ffill()
-    return data
-    # return data.loc[start_date:end_date]
-
-def load_all_data(blacklist):
-    print('loading quotes')
-
-    all_data = dict()
-    for symbol in symbols:
-        if symbol in blacklist:
-            print(symbol, '(blacklisted)')
-            continue
-
-        data = load_data(range_dates[0], range_dates[1], symbol)
-        close = data['Adj Close']
-
-        if close.isnull().all():
-            print(symbol, '(empty)')
-            continue
-
-        all_data[symbol] = data
-        print(symbol, end="\r")
-
-    return all_data
+from loader import load_all_data
 
 
 conf = Configuration('..')
@@ -58,7 +25,7 @@ blacklist = set(['CAPU', 'PESA', 'PSUR', 'POLL'])
 
 print("using dates [%s - %s]" % range_dates)
 
-all_data = load_all_data(blacklist)
+all_data = load_all_data(prices_manager, blacklist, symbols, range_dates)
 
 class ReturnsManager:
     def __init__(self, symbols, days_range, min_days):
---
+++ quotes/loader.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+
+def load_data(prices_manager, start_date, end_date, symbol):
+    data = prices_manager.load(symbol)
+    all_weekdays = pd.date_range(start=start_date, end=end_date, freq='B')
+    data = data.reindex(all_weekdays).ffill()
+    return data
+    # return data.loc[start_date:end_date]
+
+def load_all_data(prices_manager, blacklist, symbols, range_dates):
+    print('loading quotes')
+
+    all_data = dict()
+    for symbol in symbols:
+        if symbol in blacklist:
+            print(symbol, '(blacklisted)')
+            continue
+
+        data = load_data(prices_manager, range_dates[0], range_dates[1], symbol)
+        close = data['Adj Close']
+
+        if close.isnull().all():
+            print(symbol, '(empty)')
+            continue
+
+        all_data[symbol] = data
+        print(symbol, end="\r")
+
+    return all_data
